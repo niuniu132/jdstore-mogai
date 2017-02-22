d433c7b7717ddda32dca65d063cbcd946eb66311
liao <461389537@qq.com>
2017-02-22 08:47:53 +0800

    shutdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,5 +23,23 @@ class ApplicationController < ActionController::Base
      return cart
    end
 
+  def resource_name
+    :user
+  end
+  helper_method :resource_name
 
-end
+  def resource
+    @resource ||= User.new
+  end
+  helper_method :resource
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  helper_method :devise_mapping
+
+  def resource_class
+    User
+  end
+  helper_method :resource_class
+  end
